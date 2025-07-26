4795fb63cfa391d7b979b3154f1e77bf14e2ebb0
Tal Kohavy <talkohavy@gmail.com>
2025-07-26 12:30:24 +0300

    development

--- build.config.mjs
+++ build.config.mjs
@@ -1,7 +1,7 @@
-import { execSync } from "child_process";
-import fs, { cpSync } from "fs";
-import path from "path";
-import { build } from "esbuild";
+import { execSync } from 'child_process';
+import { build } from 'esbuild';
+import fs, { cpSync } from 'fs';
+import path from 'path';
 
 /**
  * @typedef {{
@@ -20,12 +20,12 @@ import { build } from "esbuild";
 
 const ROOT_PROJECT = process.cwd();
 const mode = process.env.NODE_ENV;
-const isProd = mode === "production";
-const outDirName = "dist";
+const isProd = mode === 'production';
+const outDirName = 'dist';
 const COLORS = {
-  green: "[32m",
-  blue: "[34m",
-  stop: "[39m",
+  green: '[32m',
+  blue: '[34m',
+  stop: '[39m',
 };
 
 buildPackageConfig();
@@ -39,13 +39,11 @@ async function buildPackageConfig() {
 
   manipulatePackageJsonFile();
 
-  console.log("DONE !!!");
+  console.log('DONE !!!');
 }
 
 function cleanDistDirectory() {
-  console.log(
-    `${COLORS.green}- Step 1:${COLORS.stop} clear the ${outDirName} directory`
-  );
+  console.log(`${COLORS.green}- Step 1:${COLORS.stop} clear the ${outDirName} directory`);
   execSync(`rm -rf ${outDirName}`);
 }
 
@@ -53,17 +51,17 @@ async function runBuild() {
   console.log(`${COLORS.green}- Step 2:${COLORS.stop} build the output dir`);
 
   await build({
-    entryPoints: ["src/index.ts"],
+    entryPoints: ['src/index.ts'],
     bundle: true,
     outfile: `${outDirName}/index.js`,
     sourcemap: !isProd, // <--- defaults to `false`. for 'node', create sourcemaps is for development only.
     minify: isProd, // <--- defaults to `false`. should be `true` only in production.
-    platform: "node", // <--- defaults to 'browser'. If you're creating a CLI tool, use 'node' value. Setting platform to 'node' is beneficial when for example, all packages that are built-in to node such as fs are automatically marked as external so esbuild doesn't try to bundle them.
-    format: "esm", // <--- When platform is set to 'node', this defaults to 'cjs'.
-    tsconfig: "tsconfig.json", // <--- Normally the build API automatically discovers tsconfig.json files and reads their contents during a build. However, you can also configure a custom tsconfig.json file to use instead. This can be useful if you need to do multiple builds of the same code with different settings.
+    platform: 'node', // <--- defaults to 'browser'. If you're creating a CLI tool, use 'node' value. Setting platform to 'node' is beneficial when for example, all packages that are built-in to node such as fs are automatically marked as external so esbuild doesn't try to bundle them.
+    format: 'esm', // <--- When platform is set to 'node', this defaults to 'cjs'.
+    tsconfig: 'tsconfig.json', // <--- Normally the build API automatically discovers tsconfig.json files and reads their contents during a build. However, you can also configure a custom tsconfig.json file to use instead. This can be useful if you need to do multiple builds of the same code with different settings.
     treeShaking: true, // <--- defaults to `true`. Removes dead code.
-    mainFields: ["main", "module"], // <--- When platform is set to 'node', this defaults to 'module','main'. When platform is set to 'browser', this defaults to 'browser','module','main'. IMPORTANT! The order matters! 'main', 'module' is not the same as 'module', 'main'! I chose the more risky one, that attempts to tree-shake, but could potentially fail.
-    packages: "external", // <--- You also may not want to bundle your dependencies with esbuild. There are many node-specific features that esbuild doesn't support while bundling such as __dirname, import.meta.url, fs.readFileSync, and *.node native binary modules. You can exclude all of your dependencies from the bundle by setting packages to external. If you do this, your dependencies must still be present on the file system at run-time since they are no longer included in the bundle.
+    mainFields: ['main', 'module'], // <--- When platform is set to 'node', this defaults to 'module','main'. When platform is set to 'browser', this defaults to 'browser','module','main'. IMPORTANT! The order matters! 'main', 'module' is not the same as 'module', 'main'! I chose the more risky one, that attempts to tree-shake, but could potentially fail.
+    packages: 'external', // <--- You also may not want to bundle your dependencies with esbuild. There are many node-specific features that esbuild doesn't support while bundling such as __dirname, import.meta.url, fs.readFileSync, and *.node native binary modules. You can exclude all of your dependencies from the bundle by setting packages to external. If you do this, your dependencies must still be present on the file system at run-time since they are no longer included in the bundle.
     conditions: [], // <--- If no custom conditions are configured, the Webpack-specific module condition is also included. The module condition is used by package authors to provide a tree-shakable ESM alternative to a CommonJS file without creating a dual package hazard. You can prevent the module condition from being included by explicitly configuring some custom conditions (even an empty list).
     /**
      * Some npm packages you want to use may not be designed to be run in the browser.
@@ -80,56 +78,37 @@ function copyStaticFiles() {
   console.log(`${COLORS.green}- Step 3:${COLORS.stop} copy static files`);
 
   const filesToCopyArr = [
-    { filename: "package.json", sourceDirPath: [], destinationDirPath: [] },
-    { filename: ".npmignore", sourceDirPath: [], destinationDirPath: [] },
+    { filename: 'package.json', sourceDirPath: [], destinationDirPath: [] },
+    { filename: '.npmignore', sourceDirPath: [], destinationDirPath: [] },
     {
-      filename: ".npmrc",
+      filename: '.npmrc',
       sourceDirPath: [],
       destinationDirPath: [],
       isAllowedToFail: true,
     },
-    { filename: "README.md", sourceDirPath: [], destinationDirPath: [] },
+    { filename: 'README.md', sourceDirPath: [], destinationDirPath: [] },
   ];
 
-  filesToCopyArr.forEach(
-    ({ filename, sourceDirPath, destinationDirPath, isAllowedToFail }) => {
-      try {
-        const sourceFileFullPath = path.resolve(
-          ROOT_PROJECT,
-          ...sourceDirPath,
-          filename
-        );
-        const destinationFileFullPath = path.resolve(
-          ROOT_PROJECT,
-          outDirName,
-          ...destinationDirPath,
-          filename
-        );
+  filesToCopyArr.forEach(({ filename, sourceDirPath, destinationDirPath, isAllowedToFail }) => {
+    try {
+      const sourceFileFullPath = path.resolve(ROOT_PROJECT, ...sourceDirPath, filename);
+      const destinationFileFullPath = path.resolve(ROOT_PROJECT, outDirName, ...destinationDirPath, filename);
 
-        cpSync(sourceFileFullPath, destinationFileFullPath);
-        console.log(`    • ${filename}`);
-      } catch (error) {
-        console.error(error);
-        if (isAllowedToFail) return;
+      cpSync(sourceFileFullPath, destinationFileFullPath);
+      console.log(`    • ${filename}`);
+    } catch (error) {
+      console.error(error);
+      if (isAllowedToFail) return;
 
-        throw new Error(
-          "File MUST exists in order to PASS build process! cp operation failed..."
-        );
-      }
+      throw new Error('File MUST exists in order to PASS build process! cp operation failed...');
     }
-  );
+  });
 }
 
 function manipulatePackageJsonFile() {
-  console.log(
-    `${COLORS.green}- Step 4:${COLORS.stop} copy & manipulate the package.json file`
-  );
+  console.log(`${COLORS.green}- Step 4:${COLORS.stop} copy & manipulate the package.json file`);
 
-  const packageJsonPath = path.resolve(
-    ROOT_PROJECT,
-    outDirName,
-    "package.json"
-  );
+  const packageJsonPath = path.resolve(ROOT_PROJECT, outDirName, 'package.json');
 
   // Step: get the original package.json file
   /** @type {PackageJson} */
@@ -138,16 +117,12 @@ function manipulatePackageJsonFile() {
   delete packageJson.private;
   delete packageJson.scripts;
   delete packageJson.devDependencies;
-  packageJson.publishConfig.access = "public";
+  packageJson.publishConfig.access = 'public';
   fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson));
 
   console.log(`  • ${COLORS.blue}changed${COLORS.stop} from private to public`);
   console.log(`  • ${COLORS.blue}deleted${COLORS.stop} "scripts" key`);
   console.log(`  • ${COLORS.blue}deleted${COLORS.stop} "devDependencies" key`);
-  console.log(
-    `  • ${COLORS.blue}changed${COLORS.stop} publishConfig access to public`
-  );
-  console.log(
-    `  • ${COLORS.blue}package.json${COLORS.stop} file written successfully!`
-  );
+  console.log(`  • ${COLORS.blue}changed${COLORS.stop} publishConfig access to public`);
+  console.log(`  • ${COLORS.blue}package.json${COLORS.stop} file written successfully!`);
 }
